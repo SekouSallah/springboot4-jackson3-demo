e351389ef65befa62c306478507e4813eb56704c
Sékou Sallah Sow <sowsekou@hotmail.com>
2025-12-02 21:01:04 +0000

    Initial commit

---
+++ src/main/java/dev/sekousow/springboot4jackson3demo/Springboot4Jackson3DemoApplication.java
@@ -0,0 +1,13 @@
+package dev.sekousow.springboot4jackson3demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot4Jackson3DemoApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(Springboot4Jackson3DemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/sekousow/springboot4jackson3demo/Springboot4Jackson3DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sekousow.springboot4jackson3demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot4Jackson3DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
